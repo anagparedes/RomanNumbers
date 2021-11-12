87d8fad19ee9d7e33306be7582a86344bc289b63
anagparedes <anagparedesg@gmail.com>
2021-11-11 20:47:58 -0400

    First commit

---
+++ RomanNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RomanNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm to convert decimal into Roman numbers.");
+
+            Console.WriteLine();
+            Console.WriteLine("------||PROGRAM||------");
+            ConvertNumber();
+        }
+        static void ConvertNumber()
+        {
+            Console.Write("Write a number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write($"The number in Roman: {Roman.ToRoman(num)}");
+            Console.ReadKey();
+        }
+    }
+}
